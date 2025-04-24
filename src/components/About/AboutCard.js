f05d08b4babfcdcba201cb7cebddd6870e501581
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-04-24 20:29:23 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,17 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Mandabi Mandal </span>
+            from <span className="purple"> Delhi, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am a passionate Computer Science & Engineering student at Lovely Professional University, specializing in Python, Machine Learning, and Automation.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+              Growing up in Indian Air Force stations, I developed adaptability, discipline, and resilience. /I’ve won numerous Olympiads, competitions, and actively led school events, shaping my leadership and teamwork skills.
+            <br />
+            I am currently pursuing my B./Tech in Computer Science and Engineering from Lovely Professional University.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+              I'm committed to bridging academia and industry to drive innovation. My journey reflects a blend of technical depth, creativity, and a passion for impact. With a focus on AI and automation, I aim to create solutions that shape the future meaningfully.
           </p>
           <ul>
             <li className="about-activity">
