9591faa29e5eced96fec104f34b1b2aaba2e104d
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-04-25 13:34:56 +0530

    Update EducationCard.js

--- src/components/About/EducationCard.js
+++ src/components/About/EducationCard.js
@@ -7,7 +7,7 @@ function EducationCard({ school, duration }) {
     <Card className="education-card-view">
       <Card.Body className="d-flex align-items-center">
         <div className="me-3">
-          <FaSchool size={32} color="#6c63ff" />
+          <FaSchool size={32} color="#c770f0" />
         </div>
         <div>
           <h5 className="mb-1"><strong>{school}</strong></h5>
